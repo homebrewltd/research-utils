a8e99246ab383871dad998640d9de9f461a4af41
bachvudinh <bachvudinh02@gmail.com>
2024-07-16 17:32:21 +0700

    add github CI-CD and Minio host script

--- test_flows.py
+++ test_flows.py
@@ -1,5 +1,6 @@
 from s3helper import S3Helper,S3HelperAutoConfig,S3HelperAutoTokenizer,S3HelperAutoModelForCausalLM, s3_load_dataset
 import os
+import logging
 
 os.environ['S3_ACCESS_KEY'] = 'minioadmin'
 os.environ['S3_SECRET_KEY'] = 'minioadmin'
@@ -10,8 +11,10 @@ S3Helper()
 model_name = "jan-hq/tokenizer-tinyllama"
 # model = S3HelperAutoModelForCausalLM.from_pretrained(model_name)
 tokenizer = S3HelperAutoTokenizer.from_pretrained(model_name)
+logging.info(f"Tokenizer Loading successful: {tokenizer}")
 # print(tokenizer)
 # config = S3HelperAutoConfig.from_pretrained(model_name)
 # Make sure S3Helper is initialized and environment variables are set
 # Load a dataset from S3 bucket
 dataset = s3_load_dataset("jan-hq/test-dataset",file_format='parquet', split='train')
+logging.info(f"Dataset Loading successful")
